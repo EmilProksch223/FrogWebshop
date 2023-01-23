f89138f8160737cf9791516f46a41140c2b00df3
BuildTools <unconfigured@null.spigotmc.org>
2023-01-23 22:08:08 +0100

    update-23012023

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -47,42 +47,21 @@ function createProduct(product) {
     return cardContainer;
 }
 
-
-
-
 //funktioniert noch nicht Unterricht fragen!
 
 
-if(document.querySelector(`.index-page`)) {
-    function getAndDisplayProducts() {
-        //Create the card container with col-3 and mb-3 class
-        const cardContainer = $("<div>", {class: "col-12 col-lg-4 col-xxl-3 d-flex justify-content-center mb-3"});
-        //Create the card element
-        const card = $(`<div class="card bg-dark border border-light text-white p-3" style="width: 18rem;"></div>`);
-        //Create the image element
-        const image = $(`<img class="card-img-top border border-light rounded" height="250" src="${product.imageUrl}">`);
-        //Create the card body element
-        const cardBody = $(`<div class="card-body border rounded mt-1"></div>`);
-        //Create the name element
-        const name= $(`<h5 class="card-title text-center">${product.name}</h5>`);
-        //Create the description element
-        const description = $(`<p class="card-text">${product.description}</p>`);
-        //Create the price element
-        const price = $(`<p class="card-text">${product.price}</p>`);
-        
-        //Append the elements to the card
-        cardBody.append(name);
-        cardBody.append(description);
-        cardBody.append(price);
-        card.append(image);
-        card.append(cardBody);
-        //append the card to the container
-        cardContainer.append(card);
-        return cardContainer;
-    }
-    window.addEventListener("load", function(){
-        getAndDisplayProducts();
-    });
-}
+
+document.addEventListener("DOMContentLoaded", function() {
+    //Rufe die createProduct Funktion hier auf
+    const product = {
+        name: "Product Name",
+        description: "Product Description",
+        price: "$50",
+        imageUrl: "https://via.placeholder.com/250x250.png?text=Product+Image"
+    };
+    const card = createProduct(product);
+    // Füge das erstellte Element zu einem bestimmten Bereich auf der Seite hinzu
+    document.querySelector("#product-container").appendChild(card[0]);
+});
 
 
