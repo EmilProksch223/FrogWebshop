7d33d272ab1d82df3afaaf6b4fd241a1f4c1ee65
BuildTools <unconfigured@null.spigotmc.org>
2023-06-03 18:32:42 +0200

    overlay updated

---
+++ FrontEnd/components/navbar/loadOverlay.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('/frontend/components/navbar/overlay.html')
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('overlay').innerHTML = data;
+    });
+});
