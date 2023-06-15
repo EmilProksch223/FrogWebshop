f879466a79a3d21ebd7cdc91eb8f5734481e0b01
BuildTools <unconfigured@null.spigotmc.org>
2023-06-15 19:46:02 +0200

    password encoder

--- FrontEnd/scripts/products.js
+++ FrontEnd/scripts/products.js
@@ -34,19 +34,48 @@ function createProduct(product) {
       <p>&#128220;</p>
     </button>
   </div>`);
-  const description = $(`<div class="modal fade" id="descriptionModal-${product.id}" tabindex="-1" aria-labelledby="descriptionModalLabel-${product.id}" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content bg-dark text-white">
-              <div class="modal-header">
-                <h5 class="modal-title" id="descriptionModalLabel-${product.id}">Beschreibung</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <p>${product.description}</p>
+  const details = $(`<div class="modal modal-lg fade" id="descriptionModal-${product.id}" tabindex="-1" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content bg-dark text-white">
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-4">
+            <div class="d-flex align-items-start h-100">
+              <img src="${product.imageUrl}" alt="${product.name}"
+                class="img-fluid border border-light">
+            </div>
+          </div>
+          <div class="col-8 d-flex flex-column">
+            <div>
+              <h3 class="modal-title" id="descriptionModalLabel-${product.id}">${product.name}</h3>
+            </div>
+            <div class="mt-3">
+              <p>${product.description}</p>
+            </div>
+            <div class="mt-auto">
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                  <p class="card-text mb-0">Preis: ${product.price.toFixed(2)} €</p>
+                </div>
+                <div class="ms-auto">
+                  <p class="card-text mb-0">${product.quantity} Stück auf Lager</p>
+                </div>
+                <div class="ms-3">
+                  <button type="button" class="btn btn-light d-flex" id="add-to-cart-button"
+                    data-product-id="${product.id}" style="width: 1.5rem; height: 1.5rem; padding: 0;" role="button"
+                    data-toggle="tooltip" data-placement="top" title="Produkt zum Warenkorb hinzufügen">
+                    <p style="pointer-events: none;">&#x2795;</p>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>`);
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`);
   const cardFooter = $(`<div class="card-footer border border-1 border-top-1 d-flex justify-content-between">`);
   const price = $(`<p class="card-text mb-0">Preis: ${product.price.toFixed(2)} €</p>`);
   const addProduct = $(`
@@ -56,7 +85,7 @@ function createProduct(product) {
   </button>
 `);
 
-  cardBody.append(name, drop, description);
+  cardBody.append(name, drop, details);
   card.append(image, cardBody, cardFooter);
   cardContainer.append(card);
   cardFooter.append(price, addProduct)
