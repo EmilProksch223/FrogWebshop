b1cf3c2f0d245a1b5751185ef296862206821f47
David Grasser <david.grasser1997@gmail.com>
2023-07-28 13:17:49 +0200

    working AddressTable

--- BackEnd/src/main/java/at/technikumwien/webshop/model/User.java
+++ BackEnd/src/main/java/at/technikumwien/webshop/model/User.java
@@ -2,8 +2,11 @@ package at.technikumwien.webshop.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity(name = "user")
 public class User {
@@ -27,6 +30,10 @@ public class User {
 
     @Column(name = "admin", nullable = false)
     private boolean admin;
+
+    @OneToOne // Beziehung zur Address
+    @JoinColumn(name = "address_id")
+    private Address address;
 
     // /////////////////////////////////////////////////////////////////////////
     // Getters and Setters
@@ -74,5 +81,13 @@ public class User {
 
     public void setAdmin(boolean admin) {
         this.admin = admin;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
