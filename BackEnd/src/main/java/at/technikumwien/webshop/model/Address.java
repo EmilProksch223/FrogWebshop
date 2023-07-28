cba27ddc9706acbd9b8e698ad222ffe7fdb1296c
David Grasser <david.grasser1997@gmail.com>
2023-07-28 13:49:50 +0200

    test

--- BackEnd/src/main/java/at/technikumwien/webshop/model/Address.java
+++ BackEnd/src/main/java/at/technikumwien/webshop/model/Address.java
@@ -4,9 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
 
 @Entity(name = "address")
 public class Address {
