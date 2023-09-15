5e119fe4e45ef3b4b42fd743dcd234636a3ee7ea
BuildTools <unconfigured@null.spigotmc.org>
2023-09-15 19:00:38 +0200

    filter Products admin and user, logic service

--- BackEnd/src/main/java/at/technikumwien/webshop/controller/ProductController.java
+++ BackEnd/src/main/java/at/technikumwien/webshop/controller/ProductController.java
@@ -1,7 +1,6 @@
 package at.technikumwien.webshop.controller;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,18 +26,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     /////
-    //Init
+    // Init
     /////
 
-    private ProductService service;
+    private ProductService productService;
 
-
-    public ProductController(ProductService service) {
-        this.service = service;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     /////
-    //Methods
+    // Methods
     /////
 
     @GetMapping
@@ -46,65 +44,36 @@ public class ProductController {
     public List<Product> getAllProducts(
             @RequestParam(name = "searchterm", required = false) String searchterm,
             @RequestParam(name = "activeFilter", required = false) Boolean activeFilter) {
-        List<Product> filteredProducts = new ArrayList<>();
-        List<Product> allProducts = service.getAllProducts();
-        System.out.print("Funktion");
-        if (searchterm == null && activeFilter == null) {
-            return allProducts;
-        }
-        for (Product product : allProducts) {
-            System.out.print("Filter");
 
-            if (searchterm != null && (!product.getName().toLowerCase().contains(searchterm.toLowerCase()))) {
-                continue;
-            }
-            if (activeFilter != null && product.isActive() != activeFilter) {
-                continue;
-            }
-            filteredProducts.add(product);
-        }
+        List<Product> filteredProducts = productService.getAllFilteredProdcuts(searchterm, activeFilter);
+
         return filteredProducts;
     }
 
     @GetMapping("/active")
     public List<Product> getActiveProducts(
-            @RequestParam(name = "manasymbols", required = false) String manaSymbolsString,
-            @RequestParam(name = "searchterm", required = false) String searchterm) {
-        List<Product> activeProducts = new ArrayList<>();
-        List<Product> allProducts = service.getActiveProducts();
-
-        if (manaSymbolsString == null && searchterm == null) {
-            return allProducts;
-        }
-        for (Product product : allProducts) {
-            if (manaSymbolsString != null
-                    && !product.getManaType().toLowerCase().contains(manaSymbolsString.toLowerCase())) {
-                continue;
-            }
-            if (searchterm != null && !product.getName().toLowerCase().contains(searchterm.toLowerCase())) {
-                continue;
-            }
-            activeProducts.add(product);
-        }
+            @RequestParam(name = "searchterm", required = false) String searchterm,
+            @RequestParam(name = "manasymbols", required = false) String manaSymbolsString) {
+        List<Product> activeProducts = productService.getActiveFilteredProducts(searchterm, manaSymbolsString);
         return activeProducts;
     }
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductDTO productDTO) {
-        Product product = service.save(fromDTO(productDTO));
+        Product product = productService.save(fromDTO(productDTO));
         return ResponseEntity.created(URI.create("http://localhost:8080/products")).body(product);
     }
-
+/* 
     @PutMapping("/setActive/{id}")
     public Product setActive(@PathVariable Long id) {
         return service.setActive(id);
     }
-
+*/
     @PutMapping("/update")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Product> updateProduct(@RequestBody @Valid ProductDTO productDTO) {
-        Optional<Product> optionalProduct = service.getProductById(productDTO.getId());
+        Optional<Product> optionalProduct = productService.getProductById(productDTO.getId());
         if (optionalProduct.isPresent()) {
             Product existingProduct = optionalProduct.get();
             existingProduct.setName(productDTO.getName());
@@ -114,7 +83,7 @@ public class ProductController {
             existingProduct.setQuantity(productDTO.getQuantity());
             existingProduct.setManaType(productDTO.getManaType());
             existingProduct.setActive(productDTO.isActive());
-            Product updatedProduct = service.updateProduct(existingProduct);
+            Product updatedProduct = productService.updateProduct(existingProduct);
             return ResponseEntity.ok(updatedProduct);
         } else {
             return ResponseEntity.notFound().build();
@@ -124,9 +93,9 @@ public class ProductController {
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        Optional<Product> optionalProduct = service.getProductById(id);
+        Optional<Product> optionalProduct = productService.getProductById(id);
         if (optionalProduct.isPresent()) {
-            service.deleteProductAndFile(id);
+            productService.deleteProductAndFile(id);
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
@@ -134,7 +103,7 @@ public class ProductController {
     }
 
     /////
-    //ProductDTO-Object in Product-Object
+    // ProductDTO-Object in Product-Object
     /////
 
     private static Product fromDTO(ProductDTO productDTO) {
