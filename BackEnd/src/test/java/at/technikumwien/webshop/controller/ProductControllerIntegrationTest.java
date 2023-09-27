4351ffd468692e775fff12d834fb9da4632e26d0
BuildTools <unconfigured@null.spigotmc.org>
2023-09-27 17:19:49 +0200

    integrationtest and Unitest  + maven updated files

--- BackEnd/src/test/java/at/technikumwien/webshop/controller/ProductControllerIntegrationTest.java
+++ BackEnd/src/test/java/at/technikumwien/webshop/controller/ProductControllerIntegrationTest.java
@@ -35,83 +35,86 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Import({ ProductService.class, SecurityConfig.class })
 public class ProductControllerIntegrationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @MockBean
-    private ProductRepository productRepository;
+        @MockBean
+        private ProductRepository productRepository;
 
-    @MockBean
-    private StorageService storageService;
+        @MockBean
+        private StorageService storageService;
 
-    @MockBean
-    private TokenService tokenService;
+        @MockBean
+        private TokenService tokenService;
 
-    @Test
-    @WithMockUser(roles = "ADMIN")
-    public void testGetAllFilteredProducts() throws Exception {
-        List<Product> dummyProducts = new ArrayList<>();
-        dummyProducts.add(
-                new Product("Dark Ritual", "Add 3 mana to your mana to your mana pool.", "1", 245.00, 10, "w", true));
-        dummyProducts.add(new Product("Llanoward Elves", "Add 1 Mana to your mana pool.", "3", 0.30, 15, "w", true));
-        Gson gson = new Gson();
+        @Test
+        @WithMockUser(roles = "ADMIN")
+        public void testGetAllFilteredProducts() throws Exception {
+                List<Product> dummyProducts = new ArrayList<>();
+                dummyProducts.add(
+                                new Product("Dark Ritual", "Add 3 mana to your mana to your mana pool.", "1", 245.00,
+                                                10, "w", true));
+                dummyProducts.add(new Product("Llanoward Elves", "Add 1 Mana to your mana pool.", "3", 0.30, 15, "w",
+                                true));
+                Gson gson = new Gson();
 
-        when(productRepository.findAll()).thenReturn(dummyProducts);
+                when(productRepository.findAll()).thenReturn(dummyProducts);
 
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/products?activeFilter=true&searchterm=D"))
-                .andExpect(status().isOk())
-                .andExpect(result -> gson.toJson(dummyProducts));
-    }
+                this.mockMvc.perform(MockMvcRequestBuilders
+                                .get("/products?activeFilter=true&searchterm=D"))
+                                .andExpect(status().isOk())
+                                .andExpect(result -> gson.toJson(dummyProducts));
+        }
 
-    @Test
-    @WithMockUser // No roles specified, simulating a user with no roles
-    public void testAccessRestrictedEndpointWithNoRoles() throws Exception {
-        // Execute the HTTP GET request and expect a 403 Forbidden status
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/products?activeFilter=true&searchterm=D")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isForbidden()); // Expecting a 403 Forbidden
-    }
+        @Test
+        @WithMockUser
+        public void testAccessRestrictedEndpointWithNoRoles() throws Exception {
+                mockMvc.perform(MockMvcRequestBuilders
+                                .get("/products?activeFilter=true&searchterm=D")
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isForbidden());
+        }
 
-    @Test
-    public void testGetAllActiveFilteredProducts() throws Exception {
-        List<Product> dummyProducts = new ArrayList<>();
-        dummyProducts.add(
-                new Product("Dark Ritual", "Add 3 mana to your mana to your mana pool.", "1", 245.00, 10, "w", true));
-        dummyProducts.add(new Product("Llanoward Elves", "Add 1 Mana to your mana pool.", "3", 0.30, 15, "w", true));
-        Gson gson = new Gson();
+        @Test
+        public void testGetAllActiveFilteredProducts() throws Exception {
+                List<Product> dummyProducts = new ArrayList<>();
+                dummyProducts.add(
+                                new Product("Dark Ritual", "Add 3 mana to your mana to your mana pool.", "1", 245.00,
+                                                10, "w", true));
+                dummyProducts.add(new Product("Llanoward Elves", "Add 1 Mana to your mana pool.", "3", 0.30, 15, "w",
+                                true));
+                Gson gson = new Gson();
 
-        when(productRepository.findByActive(true)).thenReturn(dummyProducts);
+                when(productRepository.findByActive(true)).thenReturn(dummyProducts);
 
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/products/active?manasymbols=w&searchterm=D"))
-                .andExpect(status().isOk())
-                .andExpect(result -> gson.toJson(dummyProducts));
-    }
+                this.mockMvc.perform(MockMvcRequestBuilders
+                                .get("/products/active?manasymbols=w&searchterm=D"))
+                                .andExpect(status().isOk())
+                                .andExpect(result -> gson.toJson(dummyProducts));
+        }
 
-    @Test
-    @WithMockUser(roles = "ADMIN")
-    public void testCreateProductAsAdmin() throws Exception {
-        ProductDTO productDTO = new ProductDTO();
-        productDTO.setActive(false);
-        productDTO.setName("testName");
-        productDTO.setDescription("testDescription");
-        productDTO.setImageUrl("1");
-        productDTO.setManaType("w");
-        productDTO.setPrice(1);
-        productDTO.setQuantity(1);
+        @Test
+        @WithMockUser(roles = "ADMIN")
+        public void testCreateProductAsAdmin() throws Exception {
+                ProductDTO productDTO = new ProductDTO();
+                productDTO.setActive(false);
+                productDTO.setName("testName");
+                productDTO.setDescription("testDescription");
+                productDTO.setImageUrl("1");
+                productDTO.setManaType("w");
+                productDTO.setPrice(1);
+                productDTO.setQuantity(1);
 
-        Product product = new Product();
-        Gson gson = new Gson();
+                Product product = new Product();
+                Gson gson = new Gson();
 
-        when(productRepository.save(any())).thenReturn(product);
+                when(productRepository.save(any())).thenReturn(product);
 
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .post("/products")
-                .content(gson.toJson(productDTO))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(result -> gson.toJson(product));
-    }
+                this.mockMvc.perform(MockMvcRequestBuilders
+                                .post("/products")
+                                .content(gson.toJson(productDTO))
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isCreated())
+                                .andExpect(result -> gson.toJson(product));
+        }
 }
